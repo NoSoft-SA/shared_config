bfeeb361bdeab96f2b9a37644e7b06625446ad2d
James Silberbauer <jamessil@telkomsa.net>
2020-02-26 15:26:38 +0200

    New variables for UD

--- shared_config.rb
+++ shared_config.rb
@@ -14,15 +14,3 @@ object = ConfigEngine.new
 DRb.start_service("druby://#{ENV.fetch('SHARED_CONFIG_HOST_PORT')}", object)
 puts 'Started...'
 DRb.thread.join
-
-# TODO: add:
-# - README
-# x VERSION
-# - git
-# - .env for host and port
-# - Gemfile
-# x Auto-start service
-# - clean shutdown on interrupt
-# - capistrano deploy
-# - on deploy, rewrite wrapper and service files with appropriate paths
-#   (symlinks an issue? for systemd?)
