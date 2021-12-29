2b3e25b1b0ebd39ed22ac9bd8b433a30144511d1
James Silberbauer <jamessil@telkomsa.net>
2021-12-29 19:12:57 +0200

    Extra variables for QC labels

--- config/deploy/ui_matrooz.rb
+++ config/deploy/ui_matrooz.rb
@@ -1,4 +1,4 @@
 # VM instance at NoSoft offices
 set :chruby_ruby, 'ruby-2.5.5'
-server '192.168.100.241', user: 'nspack', roles: %w[app db web]
+server '172.18.0.20', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/shared_config'
