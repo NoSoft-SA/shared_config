e80e6fffadf059cb42a633f8cf7d5070683ed8bc
James Silberbauer <jamessil@telkomsa.net>
2022-05-18 09:22:58 +0200

    Add packpoint, mkt addresses, phytodata and personnel name to cartons

--- config/deploy/ghs.rb
+++ config/deploy/ghs.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # GHS - Goede Hoop Sitrus
-server '192.168.6.21', user: 'ghsadmin', roles: %w[app db web]
-set :deploy_to, '/home/ghsadmin/shared_config'
+# OLD NSLD: server '192.168.6.21', user: 'ghsadmin', roles: %w[app db web]
+server '192.168.6.31', user: 'nsprint', roles: %w[app db web]
+# OLD NSLD: set :deploy_to, '/home/ghsadmin/shared_config'
+set :deploy_to, '/home/nsprint/shared_config'
 set :chruby_ruby, 'ruby-2.5.8'
