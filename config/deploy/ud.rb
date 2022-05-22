ad3b065c713b69d10a10d04ac66f2a0fb5345291
James Silberbauer <jamessil@telkomsa.net>
2022-05-22 17:52:19 +0200

    Add inspection class and drop number to carton variables

--- config/deploy/ud.rb
+++ config/deploy/ud.rb
@@ -1,4 +1,4 @@
 # VM instance at Unifrutti Dunbrody
-server '172.17.2.243', user: 'nspack', roles: %w[app db web]
+server '172.17.0.243', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/shared_config'
 set :chruby_ruby, 'ruby-2.5.5'
